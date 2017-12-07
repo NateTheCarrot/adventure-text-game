3253cec6190353d28c76d070cd76d32420433958
NateTheCarrot <34255388+NateTheCarrot@users.noreply.github.com>
2017-12-06 17:24:25 -0700

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -1,29 +1,135 @@
-﻿import time
+import time
+import sys
+import random
+from random import randint
 
 person = input("Hello there! What is your name?:\n")
-
+  
 print("Well, howdy there, {}!".format(person))
 
 time.sleep(2.5)
 print("You've been chosen to go on a special adventure. But first, tell me.\n Which class desribes you? (This is really important.)")
 time.sleep(5)
 
-print("Type in...\n\n! for warrior, you will be gifted with immense melee strength. Though, you cannot go long range...\n\n* for mage, you have been gifted with mana, a magic essence with great power. However, your mana is limited but can regenerate...\n\n+ for spy, you're skilled at being unnoticed and clever. But when caught, you may be in lots of trouble...\n\n& for pacifist, you work well with leading groups, and are skilled with making allies. However, you must be prepared for a fight...")
+print("Type in...\n\n! for warrior, you will be gifted with immense melee strength. Though, you cannot go long range...\n\n* for mage, you have been gifted with mana, a magic essence with great power. However, your mana is limited but can regenerate...\n\n+ for spy, you're skilled at being unnoticed and clever. But when caught, you may be in lots of trouble...\n\n& for pacifist, you work well with leading groups, and are skilled with making allies. However, you are unskilled in combat...")
 
 heart = input("\n")
 
-if heart == "!":
+if heart == "!":  
     print("You have chosen warrior.")
     time.sleep(5)
-    start_over = 1
-    lol = input("You've woken up with greenery everywhere you see. It is night. \nWill you...\n1. Look around.\n2. Find materials to make a fire. 3. Give it all up (pls no).\n4. Get sleep. Enter the number to choose (this goes fot responses too)")
-    if lol == "4"
-    print("You decided to go to sleep. It turns cold... You die from hypothermia.")
-    huh = input("Do you wish to try again with the same class? Yes or No.")
-    if huh == "Yes" or "yes"
-    start_over -=1
-
+    start:1
+    lol = input("You've woken up with greenery everywhere you see. It is night. \nWill you...\n1. Look around.\n2. Find materials to make a fire. \n3. Stay awake until day.\n4. Get sleep. \n\nEnter the number to choose. (this goes for all responses too.)\n")
+if lol == "4":
+    print("You decided to go to sleep. It turns cold... {} dies from hypothermia. Great job. Relaunch the whole program and get back to the previous point because the dev is to lazy to find a start over function right now. ( ͡° ͜ʖ ͡°)".format(person))
+if lol == "5":
+  print("welp, this isn't a secret exactly, the creator just needed to keep a paste bin xD: https://pastebin.com/JsePX8Ys") #< Pls no question kthx
+if lol == "3":      
 else:
     print("That isn't a class. Relaunch the program and try again.")
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#wow
+#insert meme here 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
